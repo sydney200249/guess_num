3c751e194edb835d30ee5acd6f84a021a4caadc9
sydney200249 <sydney200249@gmail.com>
2020-10-13 13:34:15 +0800

    fianl version

---
+++ guess-num.py
@@ -0,0 +1,15 @@
+import random
+r = random.randint(1,100)
+count = 0
+while True:
+	count += 1
+	num = input('請猜數字：')
+	num = int(num)
+	if num == r:
+		print('猜中了！')
+		break
+	elif num > r:
+		print('比答案大喔～')
+	elif num < r:
+		print('比答案小喔～')
+	print('你猜了第', count ,'次')
